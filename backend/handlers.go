84a4e7014f6c9a45ced0c1e1fdc79a23dac22977
Pedro Vicente <2242698@my.ipleiria.pt>
2025-06-08 09:33:58 +0100

    Refactor regex in handleClientMessage to simplify data extraction from command output

--- backend/handlers.go
+++ backend/handlers.go
@@ -306,8 +306,8 @@ func handleClientMessage(client *Client, msg ClientMessage) { // ClientMessage s
 		err = cmd.Run()            
 		stdout = outBuf.String()   
 		stderr = errBuf.String()  
-
-		re := regexp.MustCompile(`.*Data\s*=\s*\[\s*\n\s*\[\d+\]\s*\[\d+:\d+\]\s*\[DMG\]\s*([0-9]+) \(unsigned\),.*`)
+		
+		re := regexp.MustCompile(`\[DMG\]\s*([0-9]+)\s*\(unsigned\)`)
 
 		match := re.FindStringSubmatch(stdout)
 
